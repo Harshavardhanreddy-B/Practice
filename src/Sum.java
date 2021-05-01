1f02461a714e434d1f64de9a65da236f05f9cc98
harsha <harsha@DESKTOP-8H6RC7S>
2021-05-01 21:06:15 +0530

    Sum commit

---
+++ src/Sum.java
@@ -0,0 +1,12 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a = 50;
+			int b = 30;
+			int c = a+b;
+			System.out.println(c);
+	}
+
+}
